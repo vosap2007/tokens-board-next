a2225f943dc14000ff1e823cbf645ed3471c714d
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2023-04-11 14:33:54 +0300

    add start files

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const MainPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Tokens Board</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+};
+
+export default MainPage;
